29e1a70beb6784d857e28496c622fe15d149c557
rahulrathi123 <rahulrathi.1500@gmail.com>
2018-05-24 15:51:16 +0530

    indenting

--- chromeextension/background.js
+++ chromeextension/background.js
@@ -1,14 +1,12 @@
 var xmlHttp = new XMLHttpRequest();
-
-			xmlHttp.open("GET", "http://localhost:5000", true);
-			//xmlHttp.onreadystatechange = handleServerResponse;
-			console.log("reached server");
-			xmlHttp.onload = function () {
-			if (xmlHttp.readyState==4 && xmlHttp.status==200) {
-			    console.log(xmlHttp.responseText);
-        	}
+xmlHttp.open("GET", "http://localhost:5000", true);
+//console.log("reached server");
+xmlHttp.onload = function () {
+if (xmlHttp.readyState==4 && xmlHttp.status==200) {
+	console.log(xmlHttp.responseText);
+}
 };
-			xmlHttp.send(null);
+xmlHttp.send(null);
 
 
 		
