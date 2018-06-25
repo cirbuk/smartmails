56c4fc2991f3f74cd5d588e8653b4a4cabe709db
rahulrathi123 <rahulrathi.1500@gmail.com>
2018-06-25 15:23:57 +0530

    improve subjectivity model

--- myproject/app1.py
+++ myproject/app1.py
@@ -20,11 +20,43 @@ obj_clf=pickle.load(open('model/subj_clf.joblib.pkl',"rb"), encoding = "latin1")
 tone_clf=pickle.load(open('tone_model_1/subj_clf.joblib.pkl',"rb"), encoding = "latin1")
 polite_clf=pickle.load(open('politenessmodel/classifier.joblib.pkl',"rb"), encoding = "latin1")
 print('Loaded SV classifier')
+
 tf=pickle.load(open('model/vectorizer.joblib.pkl',"rb"), encoding = "latin1")
 tf_tone=pickle.load(open('tone_model_1/vectorizer.joblib.pkl',"rb"), encoding = "latin1")
 tf_polite=pickle.load(open('politenessmodel/vectorizer.joblib.pkl', "rb"), encoding = "latin1")
-print('vectorizer loaded')
 
+print('vectorizer loaded')
+wordslist = []
+classlist = []
+
+def getwordslist():
+	file = open("subjwords.txt", "r")
+	listwords = file.readlines()
+	words = []
+	classification = []
+	for item in listwords:
+		#print(item[5])
+		if item[5] == "s":
+			classification.append(item[5:15])
+		if item[5] == "w":
+			classification.append(item[5:13])
+		items = item.split()
+		words.append(items[2][6:])
+	return words, classification
+
+def buildhashtable(wordlist, classlist):
+	dic = {}
+	i = 0
+	for word in wordlist:
+		dic[word] = classlist[i]
+		i += 1
+	return dic
+
+
+
+wordslist, classlist = getwordslist()
+dic = buildhashtable(wordslist, classlist)
+#print(dic)
 
 @app.route('/postmethod', methods = ['POST'])
 def get_post_email_data():
@@ -39,23 +71,31 @@ def get_post_email_data():
 		return json.dumps({})
 	print(resList)
 	processedData=getPunctFreeString(resList)
-	word_count_length = word_count(resList).__str__()
+
+
+
+	word_count_length = word_count(resList)
 	sid=SentimentIntensityAnalyzer()
 	scores=sid.polarity_scores(processedData)
 	obj_res,obj_score=obj_clf.predict(tf.transform([processedData])),obj_clf.predict_proba(tf.transform([processedData]))
 	tone_res,tone_score=tone_clf.predict(tf_tone.transform([processedData])),tone_clf.predict_proba(tf_tone.transform([processedData]))
 	polite_res, polite_score=polite_clf.predict(tf_polite.transform([processedData])), polite_clf.predict_proba(tf_polite.transform([processedData]))
-	print(tone_res[0])
-	print(polite_res[0])
-	print(polite_score)
+	print(obj_score)
+	obj_score_modified = modifysubjscore(processedData, obj_score, word_count_length)
+	print(obj_score_modified)
+
+	#print(subj_res[0])
+	#print(subj_score)
+	#print(tone_res[0])
+	#print(polite_res[0])
+	#print(polite_score)
 	#print(tone_score)
-	doc=nlp(processedData)
 	#doc1=nlp1(processedData.decode('utf-8'))
 	scores['complex_words']=getComplexWords(resList)
 	scores['word_count']=word_count_length
 	scores['subjectivity']=round(obj_score[0,1],4)
 	scores['objectivity']=round(obj_score[0,0],4)
-	#scores['politeness']={'polite':round(polite_score[0, 0], 4), "rude": }
+	scores['politeness']={'polite':round(polite_score[0, 0], 4), "rude": round(polite_score[0, 1], 4)}
 	scores['tone']={'anger':round(tone_score[0,0],4),'fear':round(tone_score[0,1],4),'joy':round(tone_score[0,2],4),'love':round(tone_score[0,3],4),'sadness':round(tone_score[0,4],4),'surprise':round(tone_score[0,5],4)}
 	print(scores)
 	#sentiment = classifier.classify(build_bag_of_words(resList))
@@ -120,6 +160,44 @@ def getComplexWords(text):
 		if syllables>=3:
 			ncomplex=ncomplex+1
 	return ncomplex
+
+
+
+def modifysubjscore(text, score, wordcount):
+	subjlen = 0
+	objlen = 0
+	print(dic)
+	print(text)
+	words = text.split()
+	for word in words:
+		print(dic.get(word))
+		if dic.get(word) == "strongsubj":
+			subjlen += 1
+		if dic.get(word) == "weaksubj":
+			objlen += 1
+	subj_ratio = float(subjlen) / wordcount
+	obj_ratio = float(objlen) / wordcount
+	subj_diff = abs(subj_ratio - obj_ratio)
+	#print(subjlen)
+	#print(wordcount)
+	#print(subj_ratio)
+	print(subj_diff)
+	if subj_ratio > obj_ratio:
+		score[0, 1] += subj_diff
+		score[0, 0] -= subj_diff
+	else:
+		score[0, 0] += subj_diff
+		score[0, 1] -= subj_diff
+
+	if score[0,0] > 1:
+		score[0, 0] = 1
+		score[0, 1] = 0
+	if score[0, 1] > 1:
+		score[0, 1] = 1
+		score[0, 0] = 0
+	return score
+
+
 
 
 if __name__ == '__main__':
