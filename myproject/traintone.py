7b746212354e9d9b9099e95c602fba0f4861bbd7
rahulmeena661996 <rahulmeena661996@gmail.com>
2018-06-26 06:43:17 +0000

    improve tone model

--- myproject/traintone.py
+++ myproject/traintone.py
@@ -8,7 +8,7 @@ tf=TfidfVectorizer(analyzer='word')
 
 def vectorizer(data):
 	tfidf_matrix=tf.fit_transform(data)
-	pickle.dump(tf,open('tone_model_1/vectorizer.joblib.pkl',"wb"))
+	pickle.dump(tf,open('training_models/tone/vectorizer.joblib.pkl',"wb"))
 	print('vectorizer saved')
 	matrix=tfidf_matrix.toarray()
 	return matrix
@@ -29,7 +29,7 @@ def trainSVClassifier():
 			data.append(i)
 			data_labels.append('obj')
 '''
-	niters=20000
+	niters=8000
 	with open("./tone_data/anger/anger") as f:
 		j=0
 		for i in f:
@@ -84,21 +84,21 @@ def trainSVClassifier():
 		data1.append(entry[0])
 		data_labels.append(entry[1])
 
-	split=niters*0.75
+	split=int(len(data))
 
 	matrix=vectorizer(data1)
 	X_train=matrix[:split]
 	y_train=data_labels[:split]
-	X_test=matrix[split:]
-	y_test=data_labels[split:]
+	#X_test=matrix[split:]
+	#y_test=data_labels[split:]
 	print('Started training	')
 	clf_svm=SGDClassifier(loss='modified_huber', penalty='l2',alpha=1e-3,max_iter=50,tol=None,random_state=42)
 	clf_svm = clf_svm.fit(X=X_train, y=y_train)
-	predict=clf_svm.predict(X_test)
-	print(len(predict))
-	print(accuracy_score(y_test,predict))
+	#predict=clf_svm.predict(X_test)
+	#print(len(predict))
+	#print(accuracy_score(y_test,predict))
 	print("Trained SV classifier")
-	pickle.dump(clf_svm,open('tone_model_1/subj_clf.joblib.pkl',"wb"))
+	pickle.dump(clf_svm,open('training_models/tone/subj_clf.joblib.pkl',"wb"))
 trainSVClassifier()
 
 '''anger-1 fear-2 joy-3 surprise-6 5-sadness 4-love
